08bc49a8d7498c5971fcff0115051b8995985ba3
Nolan Welch <nolanwelch@outlook.com>
2025-06-15 23:24:37 -0400

    Save stimuli to disk

--- cs_alignment_llms/ingest_stimuli.py
+++ cs_alignment_llms/ingest_stimuli.py
@@ -1,5 +1,7 @@
 import re
 
+import pandas as pd
+
 
 def main():
     with open("materials.txt", "r") as f:
@@ -12,20 +14,23 @@ def main():
     # Valid stimuli should have "acceptable"/"aceptable"
     matches = [m for m in matches if re.search(r"[Aa]cc?eptable", m)]
     matches = [str(m).split("\n")[0].strip() for m in matches]
-    
+
     first_stim_idx = -1
     for idx, stim in enumerate(matches):
         if stim == "Su hermano has trained at the gym every day.":
             first_stim_idx = idx
             break
-    
+
     if first_stim_idx == -1:
         raise ValueError("Could not find first stimulus")
-    
+
     matches = matches[first_stim_idx:]
-    
-    print(matches[99])
-    
+
+    match_info = [
+        {"stimulus_number": idx + 1, "text": stimulus}
+        for idx, stimulus in enumerate(matches)
+    ]
+    pd.DataFrame(match_info).to_csv("stimuli.csv", index=False)
 
 
 if __name__ == "__main__":
